f320251d85036841c50498e1aa0b0360ce1b4c4c
0xEc4y <etstarks@proton.me>
2023-12-14 23:55:52 -0800

    Initial commit

---
+++ day01/day01.nim
@@ -0,0 +1,76 @@
+import std/strformat, std/tables, std/streams, std/strutils, std/algorithm
+
+# Achieved first AoC gold star with this =D !!
+# More to come!
+
+# Will finish part 2 soon
+
+# initialize a total
+var total = 0
+
+var
+    firstDigit: char
+    secondDigit: char
+    doubleDigitNum: string
+    firstFound = false
+    secondFound = false
+
+# equivalent to hash map
+let word2Int = {
+  "one" : 1,
+  "two" : 2,
+  "three" : 3,
+  "four" : 4,
+  "five" : 5,
+  "six" : 6,
+  "seven" : 7,
+  "eight" : 8,
+  "nine" : 9}.toTable
+
+
+# parse single line, find first and last integer, combine to form string and add that number to total
+proc parseByLinePT1(input: string): string =
+  firstFound = false
+  secondFound = false
+
+  for currentChar in input:
+    if not firstFound:
+      if currentChar.isDigit:
+        firstDigit = currentChar
+        firstFound = true
+  for currentChar in input.reversed:
+    if not secondFound:
+      if currentChar.isDigit:
+        secondDigit = currentChar
+        secondFound = true
+
+  doubleDigitNum = &"{firstDigit}{secondDigit}"
+  total += parseInt(doubleDigitNum)
+  #echo &"first number: {firstDigit}\tsecond number: {secondDigit}\tas one: {doubleDigitNum}\ttotal: {total}"
+
+#[ proc parseByLinePT2(input: string) string =
+  firstFound = false
+  secondFound = false
+
+  for currentChar in input:
+    if not firstFound:
+      if input.find(word2Int[i]) not -1 or:
+
+]#
+
+when isMainModule:
+  let contents = newFileStream("input.txt", fmRead)
+  var newLine = ""
+  # loop entire file string, parse function finds first and last int in line,
+  # and combines into two-digit int
+
+  if not isNil(contents):
+    # comments are numbering loop for debugging
+    #var i = 0
+    while contents.readLine(newLine):
+      #if i < 10:
+      discard parseByLinePT1(newLine)
+      stdout.write &"{newLine}\ttotal: {total}\n"
+      #else:
+      #  quit()
+      #i.inc
